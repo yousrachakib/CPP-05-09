d8f6763baea5eb9116158c6cf825991f1504916e
Yousra <110408013+yousrachakib@users.noreply.github.com>
2024-01-28 18:28:59 +0100

    DONEEEE CPP07

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: yochakib <yochakib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 14:30:30 by yochakib          #+#    #+#             */
-/*   Updated: 2024/01/28 11:44:37 by yochakib         ###   ########.fr       */
+/*   Updated: 2024/01/28 16:25:29 by yochakib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@
 template <typename T>
 class Array
 {
-private:
-	T* element;
-	unsigned int size_;
-public:
-	Array();
-	Array(int unsigned n);
-	Array(const Array& other);
-	Array& operator=(const Array& other);
-	T&	   operator[](unsigned int i);
-	unsigned int size() const;
-	~Array();
+	private:
+		T* element;
+		unsigned int size_;
+	public:
+		Array();
+		Array(int unsigned n);
+		Array(const Array& other);
+		Array& operator=(const Array& other);
+		T&	   operator[](unsigned int i);
+		unsigned int size() const;
+		~Array();
 };
 
 #endif
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yochakib <yochakib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 14:30:20 by yochakib          #+#    #+#             */
-/*   Updated: 2024/01/28 11:46:16 by yochakib         ###   ########.fr       */
+/*   Updated: 2024/01/28 18:28:02 by yochakib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,31 @@
 // Default constructor:
 
 template <typename T>
-Array<T>::Array() : element(NULL) , n(0) {}
+Array<T>::Array() : element(NULL) , size_(0) {}
 
 // Parameterized constructor:
 template <typename T>
 Array<T>::Array(int unsigned n) : size_(n)
 {
 	element = new T[size_];
-	for (unsigned int i; i < size_ ; i++)
+	for (unsigned int i = 0; i < size_ ; i++)
 		element[i] = T();
 }
 
 // Copy constructor :
-template <typename T> //check with peers if needs deep copy ?
+template <typename T>
 Array<T>::Array(const Array& other) : size_(other.size_)
 {
-	element = new T[size_];
-	for (unsigned int i = 0; i < size_ ; i++)
-		element[i] = other.element[i];
+	puts("hi from copy constructor");
+	element = NULL;
+	*this = other;
+}
+
+// Destructor :
+template <typename T>
+Array<T>::~Array()
+{
+	delete[] element;
 }
 
 // Assignement operator :
@@ -64,4 +71,39 @@ template <typename T>
 unsigned int Array<T>::size() const
 {
 	return (size_);
+}
+
+
+int main() 
+{
+   try
+   {
+		Array<int> arr1(5);  // Create an array of 5 integers
+		
+		for (unsigned int i = 0; i < arr1.size(); i++)
+			arr1[i] = i + 1;  // Assign values to the array elements
+
+		Array<int> arr2 = arr1;  // Copy constructor
+
+		arr2[2] = 10;  // Modify arr2, arr1 remains unchanged
+
+		std::cout << "arr1: ";
+		
+		for (unsigned int i = 0; i < arr1.size(); i++)
+			std::cout << arr1[i] << " ";
+		std::cout << std::endl;
+
+		std::cout << "arr2: ";
+		for (unsigned int i = 0; i < arr2.size(); i++)
+			std::cout << arr2[i] << " ";
+		std::cout << std::endl;
+
+		std::cout << arr2[7] << std::endl; // exception case 
+
+   }
+   catch(const std::exception& e)
+   {
+		std::cerr << "index out of range !"  << std::endl;
+   }
+    return (0);
 }

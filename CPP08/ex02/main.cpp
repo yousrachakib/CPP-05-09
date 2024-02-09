90e45fcc3c6f56ec4534d7c69b2cf1dad5927be8
Yousra <110408013+yousrachakib@users.noreply.github.com>
2024-02-09 15:54:29 +0100

    done

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -6,24 +6,24 @@
 /*   By: yochakib <yochakib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 14:57:38 by yochakib          #+#    #+#             */
-/*   Updated: 2024/02/09 15:36:40 by yochakib         ###   ########.fr       */
+/*   Updated: 2024/02/09 15:54:08 by yochakib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
-	// std::cout << mstack.top() << std::endl;
+	std::cout << mstack.top() << std::endl;
 	mstack.pop();
 	std::cout << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-//[...]
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
@@ -35,5 +35,29 @@ int main()
 		++it;
 	}
 	std::stack<int> s(mstack);
+	std::cout << "--------------------------------------------" << std::endl;
+	{
+
+        std::list<int> mstack;
+        mstack.push_back(5);
+        mstack.push_back(17);
+        std::cout << mstack.back() << std::endl;
+        mstack.pop_back();
+        std::cout << mstack.size() << std::endl;
+        mstack.push_back(3);
+        mstack.push_back(5);
+        mstack.push_back(737);
+        mstack.push_back(0);
+        std::list<int>::iterator it = mstack.begin();
+        std::list<int>::iterator ite = mstack.end();
+        ++it;
+        --it;
+        while (it != ite)
+        {
+        std::cout << *it << std::endl;
+        ++it;
+        }
+        std::list<int> s = mstack;
+    }
 	return 0;
 }
